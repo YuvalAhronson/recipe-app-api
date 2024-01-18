f144481020fced628c8336ab2d3c4da2eefda687
Yuval Ahronson <yuvalahronson@Yuvals-MacBook-Air.local>
2024-01-18 08:35:05 +0200

    added db postgresql and relevant dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator 
+"""
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def substract(x, y):
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Tests the calc module."""
+
+    def test_add_number(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test substracting numbers."""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
