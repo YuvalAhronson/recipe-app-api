2d5a73326bfd7fc3f0dd696eeffddbf7176557b1
Yuval Ahronson <yuvalahronson@Yuvals-MacBook-Air.local>
2024-02-02 17:53:56 +0200

    changed wait_for_db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=["default"])
+                self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write("Database unavaliable, waiting for 1 seconds..,")
